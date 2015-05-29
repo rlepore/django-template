b8d3f36fceba1e0d51a89977cfe3afac176319b6
Ralph Lepore <ralphlepore@gmail.com>
2015-05-28 21:45:53 -0400

    Removed the APP_DIRS setting from the TEMPLATES list.

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -61,7 +61,6 @@ TEMPLATES = [
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
         ],
-        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
